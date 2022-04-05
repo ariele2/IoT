a482e1e44680f7ceed543974332977c429cb5f7a
Ariel Elya <arielelya2@gmail.com>
2022-04-05 16:23:11 +0300

    uart_cam_h7_test working now

--- uart_cam_h7_test.py
+++ uart_cam_h7_test.py
@@ -1,13 +1,15 @@
 # Untitled - By: ariel - Tue Apr 5 2022
 
 import sensor, image, time
+from pyb import UART
+import pyb
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 2000)
 
-uart = UART(3, 9600, timeout_char=1000)
+uart = UART(1, 9600, timeout_char=1000)
 uart.init(9600, bits=8, parity=None, stop=1, timeout_char=1000)
 counter = 0
 clock = time.clock()
@@ -15,6 +17,11 @@ clock = time.clock()
 while(True):
     clock.tick()
     msg = "hello" + str(counter)
+    counter +=1
+    print("Sending...")
     uart.write(msg)
     sensor.skip_frames(time = 4000)
+    pyb.delay(300)
+    print("Recieveing...")
+    print(uart.read())
     print(clock.fps())
