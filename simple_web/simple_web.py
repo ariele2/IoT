751d8b2e10c94e6c26f2fea6ef862e9f6cd824da
Ariel Elya <arielelya2@gmail.com>
2022-04-18 18:02:09 +0300

    added functionality to the date picker

--- simple_web/simple_web.py
+++ simple_web/simple_web.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template, Response, request, redirect, url_for
+from flask import Flask, redirect, url_for, render_template, session
+from flask_wtf import FlaskForm
+from wtforms.fields import DateField
+from wtforms.validators import DataRequired
+from wtforms import validators, SubmitField
 import firebase_admin
-from firebase_admin import db
+from firebase_admin import db, credentials, initialize_app, storage
+import pandas as pd
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '#$%^&*'
+
 cred_obj = firebase_admin.credentials.Certificate("iotprojdb-firebase-adminsdk-q9c5k-113a48d6a7.json")
-default_app = firebase_admin.initialize_app(cred_obj, {'databaseURL':'https://iotprojdb-default-rtdb.europe-west1.firebasedatabase.app/'})
+storage_path = 'iotprojdb.appspot.com'
+firebase_path = 'https://iotprojdb-default-rtdb.europe-west1.firebasedatabase.app/'
+default_app = firebase_admin.initialize_app(cred_obj, {'databaseURL':firebase_path, 'storageBucket': storage_path})
+
 ref = db.reference("/test/action")
+bucket = storage.bucket()
+# blob = bucket.blob(fileName)
+# blob.upload_from_filename(fileName)
+
+class InfoForm(FlaskForm):
+    startdate = DateField('', format='%Y-%m-%d', validators=(validators.DataRequired(),))
+    enddate = DateField('-', format='%Y-%m-%d', validators=(validators.DataRequired(),))
+    submit = SubmitField('Generate CSV')
 
 def updateDB():
     data = ref.get()
@@ -16,16 +34,36 @@ def updateDB():
     ref.set("stop")
     return "stop"
 
-@app.route("/")  # this sets the route to this page
+def generateCSV(start_date, end_date):
+    # create the columns titles
+    pass
+    # query the firebase on the given dates
+    # store all of the data in a pandas table to generate a .csv file 
+
+@app.route("/", methods=['GET','POST'])  # this sets the route to this page
 def home():
+    form = InfoForm()
     data = ref.get()
-    return render_template("index.html", val=data)
+    if form.validate_on_submit():
+        session['startdate'] = form.startdate.data
+        session['enddate'] = form.enddate.data
+        return redirect('generateCSV')
+    return render_template("index.html", val=data, form=form)
+
 
 @app.route("/updateDB")
-def start_db():
+def update_db():
     res = updateDB()
     return redirect(url_for("home"))
-    # return render_template("index.html", val=res)
+
+
+@app.route("/generateCSV")
+def generate_csv():
+    startdate = session['startdate']
+    enddate = session['enddate']
+    res = generateCSV(startdate, enddate)
+    return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
     app.run()
