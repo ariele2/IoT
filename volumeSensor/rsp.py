02119ab9f74cc72129b32aa4f6f514e8c255d2ad
Ariel Elya <arielelya2@gmail.com>
2022-04-26 16:09:44 +0300

    volume detection should work with camera now

--- volumeSensor/rsp.py
+++ volumeSensor/rsp.py
@@ -12,7 +12,7 @@ vid = cv2.VideoCapture(0)
 while(True):
 	ret, frame = vid.read()
 	cv2.imshow("frame", frame)
-	if cv2.waitKey(5000) and 0xFF == ord('q'):
+	if cv2.waitKey(0) and 0xFF == ord('q'):
 		break
 
 vid.release()
@@ -149,7 +149,7 @@ cv2.destroyAllWindows()
 # 		# show the output image
 # 		print("Found ", len(idxs), "People")
 # 		cv2.imshow("Image"+str(curr_time), image)
-# 		cv2.waitKey(5000)
+# 		cv2.waitKey(1)
 # 		cv2.destroyAllWindows()
 
 # cam.release()
