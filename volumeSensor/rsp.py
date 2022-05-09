1aead350bdabe4a92b9eb83ee3dfe9a72b5ad103
noamduvd4 <noam.duvdevani@gmail.com>
2022-05-09 17:16:42 +0300

    datetime

--- volumeSensor/rsp.py
+++ volumeSensor/rsp.py
@@ -199,8 +199,7 @@ while(True):
 		response = c.request('il.pool.ntp.org', version=3)
 		ntp_time = response.tx_time
 		date = datetime.datetime.fromtimestamp(ntp_time ).strftime('%d-%m-%y %H:%M:%S')  
-		call_id = data_ref.get()
-		print("call_id: ", call_id)
+		call_id = data_ref.get()["call_id"]
 		ref_data.set({"call_id":call_id+1})
 		insert_data = {date + " " + sensor_name: {"value":str(num_of_pepole), "callID":str(call_id)}}
 		insert_real_data = {sensor_name:{"value":str(num_of_pepole), "callID":str(call_id), "time":date}}
