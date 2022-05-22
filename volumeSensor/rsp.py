0ec5944b6dabbf87f84e3b977fb7c8cb5abeb7a1
Ariel Elya <arielelya2@gmail.com>
2022-05-22 14:50:39 +0300

    fix

--- volumeSensor/rsp.py
+++ volumeSensor/rsp.py
@@ -149,7 +149,7 @@ while(True):
 		# construct a blob from the input image and then perform a forward
 		# pass of the YOLO object detector, giving us our bounding boxes and
 		# associated probabilities
-		blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),	# changed from 416 to 256 reduces time *2
+		blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (256, 256),	# changed from 416 to 256 reduces time *2
 			swapRB=True, crop=False)
 		net.setInput(blob)
 		start = time.time()
